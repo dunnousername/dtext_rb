149db75ac3558d6bceb3090a029853ba9af6c7a1
dunnousername <12957182+dunnousername@users.noreply.github.com>
2020-03-14 19:54:06 -0400

    Removed a line that caused the gem to fail to install

--- ext/dtext/extconf.rb
+++ ext/dtext/extconf.rb
@@ -3,8 +3,6 @@ require "mkmf"
 $warnflags = "-Wall -Wextra -Wno-unused-parameter"
 $CFLAGS << " -std=c99 -D_GNU_SOURCE #{ENV["CFLAGS"]}"
 
-$CFLAGS << " " << (pkg_config "glib-2.0", "cflags")
-
 have_library "glib-2.0"
 have_header "glib.h"
 have_header "dtext.h"
